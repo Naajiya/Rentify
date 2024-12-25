8e26e38169bc2a5aa65943c4696c867993bb393e
NAJIYA KP <najiyabinthmajeed@gmail.com>
2024-12-24 21:16:29 -0800

    code updated

--- src/components/SubHead.jsx
+++ src/components/SubHead.jsx
@@ -4,7 +4,12 @@ import Button from 'react-bootstrap/Button';
 function SubHead() {
 
   const [isHoveredMen, setIsHoveredMen] = useState(false);
-  const [isHoverWomen, setIsHoverWomen] = useState(false)
+  const [isHoverWomen, setIsHoverWomen] = useState(false);
+  const [isHoverVehicle, setIsHoveredVehicle] = useState(false);
+  const [isHoveredConstr, setIsHoveredConstr] = useState(false);
+  const [isHoveredDecor, setIsHoveredDecor] = useState(false)
+  const [isHoveredFurnt, setIsHoveredFurnt] = useState(false)
+  const [isHoveredKitchen, setIsHoveredKitchen] = useState(false)
   return (
     <>
       <div className='d-flex justify-content-center  hover-contaier mt-2 me-4 ms-4 rounded-3' style={{ backgroundColor: 'rgb(243, 243, 243)' }}>
@@ -22,14 +27,14 @@ function SubHead() {
         {
           isHoveredMen &&
           <div
-            className='dropdown-items border rounded p-3 bg-secondary  w-75'
+            className='dropdown-items border rounded p-3  w-75'
             onMouseEnter={() => setIsHoveredMen(true)}
             onMouseLeave={() => setIsHoveredMen(false)}
           >
-           <div >
+            <div >
               <p>items2</p>
               <p>items3</p>
-           </div>
+            </div>
           </div>
         }
 
@@ -40,22 +45,96 @@ function SubHead() {
         {
           isHoverWomen &&
           <div
-            className='dropdown-items border rounded p-3 bg-secondary  w-75'
+            className='dropdown-items border rounded p-3  w-75'
             onMouseEnter={() => setIsHoverWomen(true)}
             onMouseLeave={() => setIsHoverWomen(false)}
           >
-           <div >
+            <div >
               <p>itemfafaf</p>
               <p>items3</p>
-           </div>
+            </div>
           </div>
         }
 
-        <Button className='border border-light fw-bold  m-1' variant="">VEHICLES</Button>
-        <Button className='border border-light fw-bold m-1' variant="">BUUILDING CONSTRUCTORS</Button>
-        <Button className='border border-light fw-bold m-1' variant="">DECORATION</Button>
-        <Button className='border border-light fw-bold m-1' variant="">FURNITURE</Button>
-        <Button className='border border-light fw-bold m-1' variant="">KITCHEN</Button>
+        <Button className='border border-light fw-bold hover-button  m-1' onMouseEnter={() => setIsHoveredVehicle(true)}
+          onMouseLeave={() => setIsHoveredVehicle(false)} variant="">VEHICLES</Button>
+        {
+          isHoverVehicle &&
+          <div
+            className='dropdown-items border rounded p-3  w-75'
+            onMouseEnter={() => setIsHoveredVehicle(true)}
+            onMouseLeave={() => setIsHoveredVehicle(false)}
+          >
+            <div >
+              <p>itemfafaf</p>
+              <p>items3</p>
+            </div>
+          </div>
+        }
+
+        <Button className='border border-light fw-bold m-1' onMouseEnter={() => setIsHoveredConstr(true)}
+          onMouseLeave={() => setIsHoveredConstr(false)} variant="">BUUILDING CONSTRUCTORS</Button>
+        {
+          isHoveredConstr &&
+          <div
+            className='dropdown-items border rounded p-3  w-75'
+            onMouseEnter={() => setIsHoveredConstr(true)}
+            onMouseLeave={() => setIsHoveredConstr(false)}
+          >
+            <div >
+              <p>itemfafaf</p>
+              <p>items3</p>
+            </div>
+          </div>
+        }
+
+        <Button className='border border-light fw-bold m-1' onMouseEnter={() => setIsHoveredDecor(true)}
+          onMouseLeave={() => setIsHoveredDecor(false)} variant="">DECORATION</Button>
+        {
+          isHoveredDecor &&
+          <div
+            className='dropdown-items border rounded p-3  w-75'
+            onMouseEnter={() => setIsHoveredDecor(true)}
+            onMouseLeave={() => setIsHoveredDecor(false)}
+          >
+            <div >
+              <p>itemfafaf</p>
+              <p>items3</p>
+            </div>
+          </div>
+        }
+
+        <Button className='border border-light fw-bold m-1' onMouseEnter={() => setIsHoveredFurnt(true)}
+          onMouseLeave={() => setIsHoveredFurnt(false)} variant="">FURNITURE</Button>
+        {
+          isHoveredFurnt &&
+          <div
+            className='dropdown-items border rounded p-3  w-75'
+            onMouseEnter={() => setIsHoveredFurnt(true)}
+            onMouseLeave={() => setIsHoveredFurnt(false)}
+          >
+            <div >
+              <p>itemfafaf</p>
+              <p>items3</p>
+            </div>
+          </div>
+        }
+
+        <Button className='border border-light fw-bold m-1' onMouseEnter={() => setIsHoveredKitchen(true)}
+          onMouseLeave={() => setIsHoveredKitchen(false)} variant="">KITCHEN</Button>
+        {
+          isHoveredKitchen &&
+          <div
+            className='dropdown-items border rounded p-3  w-75'
+            onMouseEnter={() => setIsHoveredKitchen(true)}
+            onMouseLeave={() => setIsHoveredKitchen(false)}
+          >
+            <div >
+              <p>itemfafaf</p>
+              <p>items3</p>
+            </div>
+          </div>
+        }
         {/* <Button className='border border-light' variant="">Primary</Button> */}
       </div>
     </>
