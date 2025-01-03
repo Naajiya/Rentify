fa52594f5e3f313b2320da9647d4ee5c54fb3207
NAJIYA KP <najiyabinthmajeed@gmail.com>
2025-01-03 07:30:54 -0800

    viewdetails updated

--- src/pages/ViewDetails.jsx
+++ src/pages/ViewDetails.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Col, Row } from 'react-bootstrap'
 import moveOne from '../assets/moveOne.png'
+import Button from 'react-bootstrap/Button';
 
 
 function ViewDetails() {
@@ -16,19 +17,25 @@ function ViewDetails() {
 
             {/* product name and price */}
             <div className='border rounded pt-2 ps-2 m-1'>
-              <p className='fs-4'>Western Wear For girl</p>
+              <p className='fs-5'>Western Wear For girl</p>
               <div className='d-flex text-bold'>
-                <div><i class="fa-solid fa-indian-rupee-sign"></i></div>
-                <p className='fw-bold'> 109<span>/day</span></p>
+                <div className='mt-2 me-2' style={{fontSize:'25px'}}><i class="fa-solid fa-indian-rupee-sign"></i></div>
+                <p className='fs-3'> 109<span>/day</span></p>
               </div>
             </div>
 
             {/* product size */}
-            <div className='border rounded pt-2 ps-2 m-1'>
-              <p className='fs-5'>Select Size</p>
+            <div className='border rounded pt-2 p-2 ps-2 m-1'>
+              <p className='fw-bold'>Select Size</p>
               <div>
-                
+              <Button className='ms-2' style={{backgroundColor:'white',borderColor:'black',color:'black'}}>M</Button>
+              <Button className='ms-2' style={{backgroundColor:'white',borderColor:'black',color:'black'}}>LG</Button>
               </div>
+            </div>
+
+            {/* product Details */}
+            <div className='border rounded pt-2 p-2 ps-2 m-1'>
+              <p className='fw-bold'>Product details</p>
             </div>
             
             </Col>
